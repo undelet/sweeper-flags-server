6e8f057b3fe19375a2c4a0fbc7e41c494ba3f5cb
Daniel Decsi <daniel.decsi@supercharge.io>
2018-08-06 12:07:55 +0200

    implement basic server that emits all incoming messages to clients

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+import io from 'socket.io';
+
+const server = io(3000, {
+  path: '/',
+  serveClient: false,
+});
+
+server.on('connect', socket => {
+  console.log('Client connected');
+
+  socket.on('message', (message: any) => {
+    console.log(`Message going through: ${JSON.stringify(message)}`);
+    server.emit('message', message);
+  });
+});
